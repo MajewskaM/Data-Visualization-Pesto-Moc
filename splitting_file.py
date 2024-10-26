488f9639ef91f54e0e4bd143d63808577b9c8327
Monika Mazella <monikamazella@wp.pl>
2024-10-26 18:38:27 +0200

    code updated with another continent to be excluded from the main file

--- splitting_file.py
+++ splitting_file.py
@@ -3,7 +3,7 @@ import pandas as pd
 file_path = 'co-emissions-per-capita.csv'
 data = pd.read_csv(file_path)
 
-continents = ['Africa', 'Asia', 'Europe', 'Asia (excl. China and India)', 'North America', 'South America', 'Oceania', 'Antarctica']
+continents = ['Africa', 'Asia', 'Europe', 'Asia (excl. China and India)', 'North America', 'South America', 'Oceania', 'Antarctica', 'North America (excl. USA)']
 
 continents_data = data[data['Entity'].isin(continents)]
 countries_data = data[~data['Entity'].isin(continents)]
@@ -11,8 +11,8 @@ countries_data = data[~data['Entity'].isin(continents)]
 continents_file_path = 'continents_data.csv'
 countries_file_path = 'countries_data.csv'
 
-continents_data.to_csv(continents_file_path, index=False)
-countries_data.to_csv(countries_file_path, index=False)
+continents_data.to_csv(continents_file_path, index=False, mode = 'w')
+countries_data.to_csv(countries_file_path, index=False, mode = 'w')
 
 print(f"Continents data saved to {continents_file_path}")
 print(f"Countries data saved to {countries_file_path}")
